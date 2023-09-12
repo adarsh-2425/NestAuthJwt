24d5a9fa0e210f8d466f8ce0c1f04692860d1167
adarsh-2425 <you@example.comadarsh.lol2425@gmail.com>
2023-09-13 04:14:17 +0530

    update

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from 'src/interfaces/user.interface';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
@@ -17,14 +18,25 @@ export class UsersService {
 
   async create(user: User): Promise<User> {
     const newUser = new this.userModel(user);
+    // Hash the password
+    newUser.password = await this.hashPassword(newUser.password);
     return await newUser.save();
   }
 
   async update(id: String, user: User): Promise<User> {
+    if (user.password) {
+      user.password = await this.hashPassword(user.password);
+    }
     return this.userModel.findByIdAndUpdate(id, user, { new: true })
   }
 
   async delete(id: String): Promise<User> {
     return this.userModel.findByIdAndDelete(id)
+  }
+
+  // Function to Hash Password using bcryptjs
+  private async hashPassword(password: string): Promise<string> {
+    const saltRounds = 10;
+    return await bcrypt.hash(password, saltRounds)
   }
 }
